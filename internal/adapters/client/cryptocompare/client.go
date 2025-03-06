d53a6e51c10bef69a501bb412ec03792578489ac
Dmitry-gopher <dimjkee.97@gmail.com>
2025-03-06 17:11:55 +0300

    some_changes

--- internal/adapters/client/cryptocompare/client.go
+++ internal/adapters/client/cryptocompare/client.go
@@ -4,67 +4,71 @@ import (
 	"context"
 	"cryptoProject/internal/entities"
 	"encoding/json"
-	"fmt"
 	"github.com/pkg/errors"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
 type CryptoCompareClient struct {
+	// поле со списком дефолтных монет
 	httpClient *http.Client
+	baseURL    string
 }
 
 func NewCryptoCompareClient() *CryptoCompareClient {
 	return &CryptoCompareClient{
 		httpClient: &http.Client{},
+		baseURL:    "https://min-api.cryptocompare.com/data/pricemulti",
 	}
 }
 
 func (c *CryptoCompareClient) GetCurrentRates(ctx context.Context, titles []string) ([]entities.Coin, error) {
 	if len(titles) == 0 {
 		return nil, errors.Wrap(entities.ErrInvalidParameter, "no titles")
+	} // возвращать не нил, а список дефолтных монет
+
+	u, err := url.Parse(c.baseURL)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse url")
+	}
+	q := u.Query()
+	q.Set("fsyms", strings.Join(titles, ","))
+	q.Set("tsyms", "USD")
+	u.RawQuery = q.Encode()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create request")
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to do request")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.Wrapf(err, "unexpected status code: %d", resp.StatusCode)
+	}
+
+	var data map[string]map[string]float64
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, errors.Wrap(err, "failed to decode response")
 	}
 
 	var coins []entities.Coin
-
-	for _, title := range titles {
-
-		url := fmt.Sprintf("https://min-api.cryptocompare.com/data/price?fsym=%s&tsyms=USD", title)
-
-		req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
-		if err != nil {
-			return nil, errors.Wrapf(entities.ErrInternal, "failed to create request for %s", title)
-		}
-
-		resp, err := c.httpClient.Do(req)
-		if err != nil {
-			return nil, errors.Wrapf(entities.ErrInternal, "failed to send request for %s", title)
-		}
-		if resp.StatusCode != http.StatusOK {
-			return nil, errors.Wrapf(entities.ErrInternal, "unexpected status code for %s: %d", title, resp.StatusCode)
-		}
-
-		var data map[string]float64
-		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-			return nil, errors.Wrapf(entities.ErrInternal, "failed to decode response for %s", title)
-		}
-
-		if err := resp.Body.Close(); err != nil {
-			return nil, errors.Wrapf(entities.ErrInternal, "failed to close response body for %s", title)
-		}
-
-		rate, ok := data["USD"]
+	for title, rates := range data {
+		rate, ok := rates["USD"]
 		if !ok {
-			return nil, errors.Wrapf(entities.ErrInternal, "no USD in response for %s", title)
+			return nil, errors.Wrapf(entities.ErrInvalidParameter, "Rate in USD isn't available for %s", title)
 		}
-
-		coin := entities.Coin{
+		coins = append(coins, entities.Coin{
 			Title:       title,
 			CurrentRate: rate,
 			Timestamp:   time.Now(),
-		}
-
-		coins = append(coins, coin)
+		})
 	}
 
 	return coins, nil
